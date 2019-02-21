0fc2f00fd2667a14e8dc71eca9164b916bb97420
Zeljko Vukovic <zeljkov@uns.ac.rs>
2019-02-21 21:05:41 +0100

    lab3-end

---
+++ lab3-end/src/main/java/jwd/wafepa/support/ActivityToActivityDto.java
@@ -0,0 +1,44 @@
+package jwd.wafepa.support;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import jwd.wafepa.model.Activity;
+import jwd.wafepa.web.dto.ActivityDTO;
+
+@Component
+public class ActivityToActivityDto 
+	implements Converter<Activity, ActivityDTO>{
+
+	@Override
+	public ActivityDTO convert(Activity activity) {
+		ActivityDTO dto = new ActivityDTO();
+		dto.setId(activity.getId());
+		dto.setName(activity.getName());
+		
+		return dto;
+	}
+
+	public List<ActivityDTO> convert(List<Activity> activities){
+		List<ActivityDTO> retVal = new ArrayList<>();
+		
+		for(Activity a : activities) {
+			ActivityDTO dto = convert(a);
+			retVal.add(dto);
+		}
+		
+		return retVal;
+	}
+}
+
+
+
+
+
+
+
+
+
